f31dabfd01be487104209bc4a751a9ea76f3f282
Carlos Henrique Moreira <84051885+CarlosHMoreira848@users.noreply.github.com>
2024-02-12 11:03:05 -0300

    Add files via upload

---
+++ ParaContador.java
@@ -0,0 +1,11 @@
+public class ParaContador {
+	public static void main(String[] args) {
+		System.out.println("Bem vindo ao programa do contador!");
+		for(int i = 0; i <= 10; i++) {
+			for(int a = 0; a <= 10; a++) {
+				System.out.print(a + " ");
+			}
+			System.out.println();
+		}
+	}
+}
